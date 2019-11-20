683c517e117b541385f048b95762620f55f0a320
Dan VC <danvc@ellenmacarthurfoundation.org>
2019-11-20 10:56:31 +0000

    Alter initial runs suite tests intial test setup

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import 
+{ render, unmountComponentAtNode } from 'react-dom';
 import App from '../App';
+
+let container = null;
+
+beforeAll(() => {
+  container = document.createElement('div');
+  render(<App />, container);
+});
 
 
 describe('Initial runs', () => {
-
-  it('Renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
   
-  it('Add\'s the h1 heading with text Traffic Lights', () => {
-    const h1 = document.createElement('h1')
-    ReactDOM.render(<App />, h1)
-    h1.innerText = 'Traffic Lights'
-    expect(h1.innerText).toEqual('Traffic Lights')
-    ReactDOM.unmountComponentAtNode(h1);
+  it('Find h1 + check txt equals Traffic Lights', () => {
+    const h1 = container.querySelector('h1')
+    expect(h1.textContent).toEqual('Traffic Lights')
+    unmountComponentAtNode(h1);
   })
 
+})
+
+afterAll(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
 })
