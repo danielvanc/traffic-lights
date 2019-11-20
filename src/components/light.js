1c542f452f7d8a61befa91aa0591c0e8557737fc
danielvanc <email@danielvanc.com>
2019-11-20 16:14:51 +0000

    Add traffic light class name

--- src/components/light.js
+++ src/components/light.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 
+const TrafficLight = styled.li`
+
+`
 class Light extends Component {
   render() {
     const { lightID, color } = this.props;
     return (
-      <li data-lightid={lightID}>{color}</li>
+      <TrafficLight
+        data-lightid={lightID}
+        className="traffic-light"
+      >
+        {color}
+      </TrafficLight>
     );
   }
 }
