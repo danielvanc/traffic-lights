8b3193ff236500e4f9a588b04ecb732d4b4c3486
Dan VC <danvc@ellenmacarthurfoundation.org>
2019-11-20 11:56:30 +0000

    Added cypress tests

---
+++ cypress/integration/app_spec.js
@@ -0,0 +1,18 @@
+describe('Ensure page runs and tests in cypress', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('Check that the app runs', () => {
+    cy
+      .get('#root')
+      .find('h1')
+  })
+  it('Check app contains h1 and contains text', () => {
+    cy.get('#root').find('h1').contains('Traffic Lights')
+  })
+  it('Check the app contains the traffic lights panel', () => {
+    cy
+      .get('root')
+      .find('.traffic-panel')
+  })
+})
